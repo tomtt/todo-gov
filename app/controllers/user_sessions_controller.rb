39b4bec483109d3b512fb08c34a5e0dca00332a7
Tom ten Thij <git@tomtenthij.nl>
2010-03-19 14:31:35 +0000

    User session stuff

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,36 @@
+class UserSessionsController < ApplicationController
+  resources_controller_for :user_session, :singleton => true, :load_enclosing => false
+
+  # before_filter :require_no_user, :only => [:new, :create]
+  before_filter :logout_if_current_user, :only => [:create]
+  before_filter :require_user, :only => :destroy
+
+  response_for :create do |format|
+    format.html do
+      if @resource_saved
+        flash[:notice] = "Login successful!"
+        redirect_back_or_default person_path(resource.person)
+      else
+        render :action => :new
+      end
+    end
+  end
+
+  def destroy
+    @last_logged_in_user_id = current_user.to_param
+    current_user_session.destroy
+  end
+
+  response_for :destroy do |format|
+    format.html do
+      flash[:notice] = "Logout successful!"
+      redirect_back_or_default person_path(@last_logged_in_user_id)
+    end
+  end
+
+  protected
+
+  def logout_if_current_user
+    current_user_session && current_user_session.destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
+  # Resources
   map.resources :lists
+  map.resource :user_session
+
+  # Named routes
+  map.login '/login', :controller => "user_sessions", :action => "new"
+  map.logout '/logout', :controller => "user_sessions", :action => "destroy"
 end
