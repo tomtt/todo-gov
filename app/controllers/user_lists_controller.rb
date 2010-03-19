993ea20d89ebba3fb55aaefb479449a1acca8d13
Tom Lea <commit@tomlea.co.uk>
2010-03-19 14:45:41 +0000

    Add user_lists.

---
+++ app/controllers/user_lists_controller.rb
@@ -0,0 +1,3 @@
+class UserListsController < ApplicationController
+  resources_controller_for :user_lists
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :lists
+  map.resources :user_lists
+  map.root :lists
 end
