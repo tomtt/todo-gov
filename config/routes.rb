00ccad34b7f6a331f937204d569a6e2c4ffc88b6
Tom Lea <commit@tomlea.co.uk>
2010-03-19 14:50:12 +0000

    Oops, bad routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.root :lists
-
   map.resources :lists
   map.resources :user_lists
   map.resource :user_session
@@ -8,4 +6,6 @@ ActionController::Routing::Routes.draw do |map|
   # Named routes
   map.login '/login', :controller => "user_sessions", :action => "new"
   map.logout '/logout', :controller => "user_sessions", :action => "destroy"
+
+  map.root :lists
 end
