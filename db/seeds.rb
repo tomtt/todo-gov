d71320e63790a99ba09d3ca4b57901c8ed2357a6
Tom Lea <commit@tomlea.co.uk>
2010-03-19 14:20:48 +0000

    Here, have so seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#   
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Major.create(:name => 'Daley', :city => cities.first)
+list = List.create!(:name => "Register a Death")
+File.read("scratch.txt").split("\n").grep(/^\[\]/).each{|v| name = v.gsub('[] ', ''); Item.create!(:name => name, :list => list) }
