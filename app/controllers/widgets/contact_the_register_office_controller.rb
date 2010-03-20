03481c9e393545686eb4ca82b9d00ee082796f96
George Brocklehurst <george.brocklehurst@gmail.com>
2010-03-20 13:16:24 +0000

    Registry office widget.

--- app/controllers/widgets/contact_the_register_office_controller.rb
+++ app/controllers/widgets/contact_the_register_office_controller.rb
@@ -1,3 +1,9 @@
-class Widgets::ContactTheRegisterOfficeController < ApplicationController
-  # Use this API: http://tartarus.org/richard/registries/docs
+require "open-uri"
+require "ostruct"
+
+class Widgets::ContactTheRegisterOfficeController < Widgets::WidgetsController
+  def create
+    api_response = JSON.parse(open("http://tartarus.org/richard/registries/find?loc=#{CGI.escape(params[:user_data][:postcode])}").read)
+    @registry_offices = api_response["registries"].map{|v| OpenStruct.new(v)}
+  end
 end
